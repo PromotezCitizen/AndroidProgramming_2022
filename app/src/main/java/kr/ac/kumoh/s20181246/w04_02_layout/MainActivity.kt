18477478527d7a09377d2f1990e6842099ba4d6e
PromotezCitizen <gkstkdgus821@gmail.com>
2022-09-27 14:34:28 +0900

    Column과 Row를 이용하여 XML에서 Linear, Vertical 한 것과 같은 효과를 낼 수 있다.

--- app/src/main/java/kr/ac/kumoh/s20181246/w04_02_layout/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20181246/w04_02_layout/MainActivity.kt
@@ -3,30 +3,76 @@ package kr.ac.kumoh.s20181246.w04_02_layout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kr.ac.kumoh.s20181246.w04_02_layout.ui.theme.W04_02_LayoutTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            W04_02_LayoutTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MyApp()
         }
     }
+}
+
+@Composable
+fun MyApp() {
+    W04_02_LayoutTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            // Greeting("인사")
+            MyLinearLayout()
+        }
+    }
+}
+
+@Composable
+fun MyLinearLayout() {
+    Column {
+        Row{
+            Text(
+                text = "안녕하세요",
+                modifier = Modifier
+                    .background(Color.Yellow)
+                    .padding(8.dp),
+                color = Color.Black
+            )
+            Text(
+                text = "한상현",
+                modifier = Modifier
+                    .background(Color.Cyan)
+                    .padding(20.dp),
+                color = Color.Black
+
+            )
+        }
+        Text(
+            text = "어디선가 본 것 같은 예제",
+            modifier = Modifier
+                .background(Color.Magenta)
+                .padding(10.dp),
+            color = Color.Black
+        )
+    }
+//    Row {
+//        Text(text = "안녕하세요")
+//        Text(text = "한상현")
+//    }
 }
 
 @Composable
@@ -37,7 +83,5 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    W04_02_LayoutTheme {
-        Greeting("Android")
-    }
+    MyApp()
 }
