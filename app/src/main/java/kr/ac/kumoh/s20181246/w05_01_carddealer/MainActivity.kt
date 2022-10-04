7d71520911c3bab081bfdbeb118a19da2fc29a8b
PromotezCitizen <gkstkdgus821@gmail.com>
2022-10-04 15:50:19 +0900

    add getCardNum. 숫자 입력 받으면 카드 모양, 해당 숫자 판별

--- app/src/main/java/kr/ac/kumoh/s20181246/w05_01_carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20181246/w05_01_carddealer/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.ac.kumoh.s20181246.w05_01_carddealer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kr.ac.kumoh.s20181246.w05_01_carddealer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +15,36 @@ class MainActivity : AppCompatActivity() {
 //        setContentView(R.layout.activity_main)
         setContentView(binding.root)
 
-        binding.card1.setImageResource(R.drawable.c_10_of_clubs)
+        getCardName(30)
 
+        binding.card1.setImageResource(R.drawable.c_10_of_clubs)
+        binding.card2.setImageResource(R.drawable.c_9_of_clubs)
+        binding.card3.setImageResource(R.drawable.c_8_of_clubs)
+        binding.card4.setImageResource(R.drawable.c_7_of_clubs)
+        binding.card5.setImageResource(R.drawable.c_6_of_clubs)
+
+    }
+
+    private fun getCardName(c: Int): String {
+        val shape = when (c / 13) {
+            0 -> "spades"
+            1 -> "diamonds"
+            2 -> "hearts"
+            3 -> "clubs"
+            else -> "error"
+        }
+        val num = when (c % 13) {
+            0 -> "ace"
+            in 1..9 -> (c%13 + 1).toString()
+            10 -> "jack"
+            11 -> "queen"
+            12 -> "king"
+            else -> "error"
+        }
+
+        Log.i("getCardName()", "Shape: ${shape}, Num: ${num}")
+
+
+        return "c_10_of_clubs"
     }
 }
