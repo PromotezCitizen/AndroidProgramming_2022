713ca5578590b7f78b213db791d634048af9b1e5
PromotezCitizen <gkstkdgus821@gmail.com>
2022-11-15 14:28:59 +0900

    Start!

---
+++ app/src/main/java/kr/ac/kumoh/s20181246/w11_02_volleywithviewmodel/MainActivity.kt
@@ -0,0 +1,14 @@
+package kr.ac.kumoh.s20181246.w11_02_volleywithviewmodel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.ac.kumoh.s20181246.w11_02_volleywithviewmodel.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
