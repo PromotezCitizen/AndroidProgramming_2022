cb58164ef29cbb9b228b65ad9b44aad4d2024f12
PromotezCitizen <gkstkdgus821@gmail.com>
2022-11-15 14:28:22 +0900

    Start!

---
+++ app/src/main/java/kr/ac/kumoh/s20181246/w11_02_volleywithviewmodel/SongViewModel.kt
@@ -0,0 +1,8 @@
+package kr.ac.kumoh.s20181246.w11_02_volleywithviewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class SongViewModel(application: Application) : AndroidViewModel(application) {
+
+}
