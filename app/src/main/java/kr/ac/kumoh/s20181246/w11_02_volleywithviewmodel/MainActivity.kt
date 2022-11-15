208bb646a95a7a5156c756ce1ae133c01c2fd866
PromotezCitizen <gkstkdgus821@gmail.com>
2022-11-15 15:17:29 +0900

    Volley 및 viewModel을 이용하여 서버에서 데이터를 받아와 출력해보자!

--- app/src/main/java/kr/ac/kumoh/s20181246/w11_02_volleywithviewmodel/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20181246/w11_02_volleywithviewmodel/MainActivity.kt
@@ -2,13 +2,36 @@ package kr.ac.kumoh.s20181246.w11_02_volleywithviewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import kr.ac.kumoh.s20181246.w11_02_volleywithviewmodel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var model: SongViewModel
+    private var songs: Array<String>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        model = ViewModelProvider(this)[SongViewModel::class.java] // 몬가... 몬가... 어렵다
+        model.requestSongs()
+
+        model.songs.observe(
+            this,
+            Observer<ArrayList<String>> {
+//                Toast.makeText(this, model.songs.value.toString().replace(" ", "\n").replace("[\\[\\]]".toRegex(), ""), Toast.LENGTH_LONG)
+                songs = model.songs.value?.toTypedArray()
+                binding.listSongs.adapter = ArrayAdapter<String>(
+                    this,
+                    android.R.layout.simple_list_item_1,
+                    songs as Array<out String>
+                )
+            }
+        )
     }
 }
--- app/src/main/java/kr/ac/kumoh/s20181246/w11_02_volleywithviewmodel/SongViewModel.kt
+++ app/src/main/java/kr/ac/kumoh/s20181246/w11_02_volleywithviewmodel/SongViewModel.kt
@@ -1,8 +1,63 @@
 package kr.ac.kumoh.s20181246.w11_02_volleywithviewmodel
 
 import android.app.Application
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONArray
+import org.json.JSONObject
 
 class SongViewModel(application: Application) : AndroidViewModel(application) {
+    companion object {
+        const val QUEUE_TAG = "SongVolleyRequest"
+    }
+    private val list = ArrayList<String>()
+    val songs = MutableLiveData<ArrayList<String>>()
 
+    private var queue: RequestQueue = Volley.newRequestQueue(getApplication())
+    // getApplication : AndroidViewModel에서 상속받는다.
+
+    init { // 변수 초기화 기능
+        songs.value = list
+    }
+
+    fun requestSongs() {
+        val url = "https://androidexpressdb-ajrmo.run.goorm.io"
+        val request = JsonArrayRequest(
+            Request.Method.GET,
+            url,
+            null,
+            {
+                list.clear()
+                parseJson(it)
+                songs.value = list
+//                Toast.makeText(getApplication(), list.toString().replace(" ", "").replace("[\\[\\]]".toRegex(), ""), Toast.LENGTH_LONG).show()
+            },
+            {
+                Toast.makeText(getApplication(), it.toString(), Toast.LENGTH_LONG).show()
+            }
+        )
+        request.tag = QUEUE_TAG
+        queue.add(request)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        queue.cancelAll(QUEUE_TAG)
+    }
+
+    private fun parseJson(items: JSONArray) {
+        for (i in 0 until items.length()){
+            val item = items[i] as JSONObject
+            val id = item.getInt("id")
+            val title = item.getString("title")
+            val singer = item.getString("singer")
+            list.add("${singer}-${title}(${id})")
+//            list.add((items[i] as JSONObject)["title"] as String + (items[i] as JSONObject)["singer"] as String)
+        }
+    }
 }
