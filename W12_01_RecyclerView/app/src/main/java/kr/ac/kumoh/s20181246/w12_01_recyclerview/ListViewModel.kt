be27a465fee6201d66f500e231d0b196fa999edd
PromotezCitizen <gkstkdgus821@gmail.com>
2022-11-22 14:53:25 +0900

    viewModel 생성

---
+++ W12_01_RecyclerView/app/src/main/java/kr/ac/kumoh/s20181246/w12_01_recyclerview/ListViewModel.kt
@@ -0,0 +1,23 @@
+package kr.ac.kumoh.s20181246.w12_01_recyclerview
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ListViewModel: ViewModel() {
+    private val songs = ArrayList<String>()
+    private val list = MutableLiveData<ArrayList<String>>()
+
+    init {
+        list.value = songs
+    }
+
+    fun getList(): LiveData<ArrayList<String>> = list
+    fun add(song: String) {
+        songs.add(song)
+        list.value = songs
+    }
+    fun getSont(i: Int) = songs[i]
+    fun getSize() = songs.size
+
+}
