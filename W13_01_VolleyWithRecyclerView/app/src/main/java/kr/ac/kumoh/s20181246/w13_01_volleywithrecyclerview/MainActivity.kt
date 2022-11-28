2197aef8dca5c990e1d46cf1e750f194cdaada00
PromotezCitizen <gkstkdgus821@gmail.com>
2022-11-28 10:21:26 +0900

    W13 start!

---
+++ W13_01_VolleyWithRecyclerView/app/src/main/java/kr/ac/kumoh/s20181246/w13_01_volleywithrecyclerview/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20181246.w13_01_volleywithrecyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
