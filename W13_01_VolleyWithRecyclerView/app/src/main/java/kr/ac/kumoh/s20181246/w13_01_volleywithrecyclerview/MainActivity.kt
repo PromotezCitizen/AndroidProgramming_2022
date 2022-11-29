ebe7cb578b4059ca36599b2bf5b9021caa2d80cf
PromotezCitizen <gkstkdgus821@gmail.com>
2022-11-29 14:24:41 +0900

    recyclerview Adapter 생성

--- W13_01_VolleyWithRecyclerView/app/src/main/java/kr/ac/kumoh/s20181246/w13_01_volleywithrecyclerview/MainActivity.kt
+++ W13_01_VolleyWithRecyclerView/app/src/main/java/kr/ac/kumoh/s20181246/w13_01_volleywithrecyclerview/MainActivity.kt
@@ -2,7 +2,11 @@ package kr.ac.kumoh.s20181246.w13_01_volleywithrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import kr.ac.kumoh.s20181246.w13_01_volleywithrecyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +18,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         model = ViewModelProvider(this)[SongViewModel::class.java]
+
+
+
         model.requestSong()
+    }
+
+    inner class SongAdapter : RecyclerView.Adapter<SongAdapter.ViewHolder>() {
+        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val txText: TextView = itemView.findViewById<TextView>(android.R.id.text1)
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view = layoutInflater.inflate(android.R.layout.simple_list_item_1, parent, false)
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.txText.text = model.list.value?.get(position).toString()
+        }
+
+        override fun getItemCount(): Int = model.list.value?.size ?: 0
     }
 }
