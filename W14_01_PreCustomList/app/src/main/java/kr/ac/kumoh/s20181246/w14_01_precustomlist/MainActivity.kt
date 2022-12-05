11a698e77b4a19d4cd94041981c75af77facfb55
PromotezCitizen <gkstkdgus821@gmail.com>
2022-12-05 10:49:16 +0900

    사용자 정의 xml layout 추가

--- W14_01_PreCustomList/app/src/main/java/kr/ac/kumoh/s20181246/w14_01_precustomlist/MainActivity.kt
+++ W14_01_PreCustomList/app/src/main/java/kr/ac/kumoh/s20181246/w14_01_precustomlist/MainActivity.kt
@@ -2,10 +2,68 @@ package kr.ac.kumoh.s20181246.w14_01_precustomlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kr.ac.kumoh.s20181246.w14_01_precustomlist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var model: SongViewModel
+    private val songAdapter = SongAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        model = ViewModelProvider(this)[SongViewModel::class.java]
+
+        binding.list.apply {
+            //layoutManager = LinearLayoutManager(applicationContext)
+            layoutManager = LinearLayoutManager(application)
+            setHasFixedSize(true)
+            itemAnimator = DefaultItemAnimator()
+            adapter = songAdapter
+        }
+
+        model.list.observe(this) {
+            songAdapter.notifyItemRangeInserted(
+                0,
+                // model.list.value?.size ?: 0)
+                // songAdapter.getItemCount())
+                songAdapter.itemCount
+            )}
+
+
+        model.requestSong()
+    }
+
+    inner class SongAdapter: RecyclerView.Adapter<SongAdapter.ViewHolder>() {
+        inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+            // val txText: TextView = itemView.findViewById(android.R.id.text1)
+            // val txText = itemView.findViewById(android.R.id.text1) as TextView
+            val txText = itemView.findViewById<TextView>(R.id.text1)
+            val txSinger = itemView.findViewById<TextView>(R.id.text2)
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view = layoutInflater.inflate(R.layout.item_song,
+                parent,
+                false)
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.txText.text = model.list.value?.get(position).toString()
+            holder.txSinger.text = model.list.value?.get(position).toString()
+        }
+
+        override fun getItemCount() = model.list.value?.size ?: 0
     }
 }
